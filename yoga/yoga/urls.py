9ec85147e5ec92bc90da097ff851afb06c9232ef
jean <jean_carlos104@hotmail.com>
2015-01-02 14:01:52 -0500

    a

--- yoga/yoga/urls.py
+++ yoga/yoga/urls.py
@@ -1,15 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from django.conf import settings
+
 urlpatterns = patterns('',
     url(r'^', include('apps.users.urls')),
     url(r'^', include('apps.classes.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-
-
 )
-
 
 if not settings.DEBUG:
     urlpatterns += patterns('',
