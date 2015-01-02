e34326075f75c1f6b3e63310e5f3b95878f27dd1
jean <jean_carlos104@hotmail.com>
2015-01-02 13:15:39 -0500

    a

--- yoga/yoga/settings/prod.py
+++ yoga/yoga/settings/prod.py
@@ -1,1 +1,21 @@
 from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Series',
+        'USER': 'cineuserprod',
+        'PASSWORD': 'cine',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+STATIC_URL = 'http://seriesparatodos.com/static/'
+MEDIA_URL = 'http://seriesparatodos.com/media/'
+STATIC_ROOT = BASE_DIR.child('static')
+MEDIA_ROOT = BASE_DIR.child('media')
