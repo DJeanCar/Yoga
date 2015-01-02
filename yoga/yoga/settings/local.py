7c262629a001a0ff8dde794c5817283e28ede120
jean <jean_carlos104@hotmail.com>
2015-01-02 11:56:13 -0500

    a

--- yoga/yoga/settings/local.py
+++ yoga/yoga/settings/local.py
@@ -3,18 +3,6 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'yoga',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-'''
 
 DATABASES = {
     'default': {
